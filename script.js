8e19ba8d61def1021e6bbe7458cb3ab1927d94ef
naimur <naimur202163@gmail.com>
2022-08-14 09:29:39 +0600

    eplanding cards are added

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ const panel=document.querySelectorAll('.panel');
 
 panel.forEach((panel)=>{
     panel.addEventListener('click',()=>{
+        removeActiveClasses()
         panel.classList.add('active')
     })
 })
 
 // console.log(panel)
+
+const removeActiveClasses=()=>{
+    panel.forEach((panel,index)=>{
+panel.classList.remove('active')
+    })
+}
